72dbcf24a00269bb0f24800538a23cd704de2c55
dnyrm <cdgmachado0@gmail.com>
2024-09-03 23:21:59 +0200

    Before adding the diamond

--- test/foundry/RouterTest.sol
+++ test/foundry/RouterTest.sol
@@ -5,28 +5,17 @@ pragma solidity >=0.8.23 <0.9.0;
 
 import {Test, console} from "../../lib/forge-std/src/Test.sol";
 import {StateVars} from "../../contracts/StateVars.sol";
-// import {IPAllActionV3} from "@pendle/core-v2/contracts/interfaces/IPAllActionV3.sol";
-// import {IPMarket} from "@pendle/core-v2/contracts/interfaces/IPMarket.sol";
 import {IStandardizedYield} from "@pendle/core-v2/contracts/interfaces/IStandardizedYield.sol";
 import {IPPrincipalToken} from "@pendle/core-v2/contracts/interfaces/IPPrincipalToken.sol";
 // import {IERC20} from "../../lib/forge-std/src/interfaces/IERC20.sol";
 
-// interface PT {
-//     function isExpired() external view returns (bool);
-// }
 
 
-contract RouterTest is Test, StateVars {
 
-    // IPAllActionV3 pendleRouter;
-    // IPMarket sUSDeMarket;
-    
+contract RouterTest is Test, StateVars {    
 
     function setUp() public {
         vm.createSelectFork(vm.rpcUrl('ethereum'), blockOwnerPT); //blockOwnerPT + 100
-
-        // pendleRouter = IPAllActionV3(router);
-        // sUSDeMarket = IPMarket(pendleMarket);
 
         deal(address(sUSDe), address(this), 1_000 * 1e18);
         sUSDe.approve(address(pendleRouter), type(uint).max);
