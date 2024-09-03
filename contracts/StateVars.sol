f32e594ed555085b9547e2d049e382fcd6df9087
dnyrm <cdgmachado0@gmail.com>
2024-09-03 23:07:40 +0200

    Done with YT swap/mint test

--- contracts/StateVars.sol
+++ contracts/StateVars.sol
@@ -4,12 +4,22 @@ pragma solidity 0.8.26;
 
 import {IERC20} from "../lib/forge-std/src/interfaces/IERC20.sol";
 import {StructGen} from "./StructGen.sol";
+import {IPMarket} from "@pendle/core-v2/contracts/interfaces/IPMarket.sol";
+import {IPAllActionV3} from "@pendle/core-v2/contracts/interfaces/IPAllActionV3.sol";
 
 
 contract StateVars is StructGen {
 
-    address public constant router = 0x888888888889758F76e7103c6CbF23ABbF58F946;
-    address public constant pendleMarket = 0xd1D7D99764f8a52Aff007b7831cc02748b2013b5;
+    address public constant ownerPT = 0x62178e35ccef8E00e33AFC95F12a590b40E51E04;
+    uint blockOwnerPT = 20468410;
+
+    uint currentBlock = 20665666;
+
+    IPAllActionV3 public constant pendleRouter = IPAllActionV3(0x888888888889758F76e7103c6CbF23ABbF58F946);
+    IPMarket public constant sUSDeMarket = IPMarket(0xd1D7D99764f8a52Aff007b7831cc02748b2013b5);
+
     IERC20 public constant sUSDe = IERC20(0x9D39A5DE30e57443BfF2A8307A4256c8797A3497);
+    IERC20 public constant sUSDe_PT_26SEP = IERC20(0x6c9f097e044506712B58EAC670c9a5fd4BCceF13);
+    IERC20 public constant YT = IERC20(0xdc02b77a3986da62C7A78FED73949C9767850809);
 
 }
--- test/foundry/RouterTest.sol
+++ test/foundry/RouterTest.sol
@@ -5,27 +5,32 @@ pragma solidity >=0.8.23 <0.9.0;
 
 import {Test, console} from "../../lib/forge-std/src/Test.sol";
 import {StateVars} from "../../contracts/StateVars.sol";
-import {IPAllActionV3} from "@pendle/core-v2/contracts/interfaces/IPAllActionV3.sol";
-import {IPMarket} from "@pendle/core-v2/contracts/interfaces/IPMarket.sol";
+// import {IPAllActionV3} from "@pendle/core-v2/contracts/interfaces/IPAllActionV3.sol";
+// import {IPMarket} from "@pendle/core-v2/contracts/interfaces/IPMarket.sol";
 import {IStandardizedYield} from "@pendle/core-v2/contracts/interfaces/IStandardizedYield.sol";
 import {IPPrincipalToken} from "@pendle/core-v2/contracts/interfaces/IPPrincipalToken.sol";
 // import {IERC20} from "../../lib/forge-std/src/interfaces/IERC20.sol";
 
+// interface PT {
+//     function isExpired() external view returns (bool);
+// }
+
 
 contract RouterTest is Test, StateVars {
 
-    IPAllActionV3 pendleRouter;
-    IPMarket sUSDeMarket;
+    // IPAllActionV3 pendleRouter;
+    // IPMarket sUSDeMarket;
     
 
     function setUp() public {
-        vm.createSelectFork(vm.rpcUrl('ethereum'), 20665666);
+        vm.createSelectFork(vm.rpcUrl('ethereum'), blockOwnerPT); //blockOwnerPT + 100
 
-        pendleRouter = IPAllActionV3(router);
-        sUSDeMarket = IPMarket(pendleMarket);
+        // pendleRouter = IPAllActionV3(router);
+        // sUSDeMarket = IPMarket(pendleMarket);
 
         deal(address(sUSDe), address(this), 1_000 * 1e18);
         sUSDe.approve(address(pendleRouter), type(uint).max);
+        YT.approve(address(pendleRouter), type(uint).max);
 
         _setLabels();
     }
@@ -53,12 +58,46 @@ contract RouterTest is Test, StateVars {
     }
 
 
+    function test_mintPT() public {
+        //Mint PT and YT
+        uint sUSDeBalance = sUSDe.balanceOf(address(this));
+        require(sUSDeBalance > 0, 'sUSDeBalance less than 0');
+
+        (uint256 netPyOut,) =
+            pendleRouter.mintPyFromToken(address(this), address(YT), 0, createTokenInputStruct(address(sUSDe), sUSDeBalance));
+
+        uint exactYtIn = YT.balanceOf(address(this));
+        console.log('netPyOut - post mint: ', netPyOut);
+        console.log('YT - post mint: ', exactYtIn);
+
+        console.log('');
+
+        //Swap YT for PT
+        pendleRouter.swapExactYtForPt(address(this), address(sUSDeMarket), exactYtIn, 0, defaultApprox);
+        console.log('PT bal - post swap: ', sUSDe_PT_26SEP.balanceOf(address(this)));
+        console.log('YT bal - post swap: ', YT.balanceOf(address(this)));
+    }
+
+
+    function test_ownerPT() public view {
+        uint ptBalance = sUSDe_PT_26SEP.balanceOf(ownerPT);
+        console.log('ptBalance - not 0: ', ptBalance);
+        
+        // console.log('isExpired: ', PT(address(sUSDe_PT_26SEP)).isExpired());
+    }
+
+
+
+
+
+
     //********* */
     
     function _setLabels() private {
         vm.label(address(pendleRouter), 'pendleRouter');
         vm.label(address(sUSDeMarket), 'sUSDeMarket');
         vm.label(address(sUSDe), 'sUSDe');
+        vm.label(ownerPT, 'ownerPT');
     }
     
 }
