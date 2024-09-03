5f2bb8e0267b77fc462524b0ddbedcddc7b6f691
dnyrm <cdgmachado0@gmail.com>
2024-09-03 18:20:24 +0200

    Test PT swap works

--- test/foundry/RouterTest.sol
+++ test/foundry/RouterTest.sol
@@ -4,7 +4,7 @@ pragma solidity >=0.8.23 <0.9.0;
 
 
 import {Test, console} from "../../lib/forge-std/src/Test.sol";
-import {Storage} from "../../contracts/Storage.sol";
+import {StateVars} from "../../contracts/StateVars.sol";
 import {IPAllActionV3} from "@pendle/core-v2/contracts/interfaces/IPAllActionV3.sol";
 import {IPMarket} from "@pendle/core-v2/contracts/interfaces/IPMarket.sol";
 import {IStandardizedYield} from "@pendle/core-v2/contracts/interfaces/IStandardizedYield.sol";
@@ -12,7 +12,7 @@ import {IPPrincipalToken} from "@pendle/core-v2/contracts/interfaces/IPPrincipal
 // import {IERC20} from "../../lib/forge-std/src/interfaces/IERC20.sol";
 
 
-contract RouterTest is Test, Storage {
+contract RouterTest is Test, StateVars {
 
     IPAllActionV3 pendleRouter;
     IPMarket sUSDeMarket;
@@ -26,6 +26,8 @@ contract RouterTest is Test, Storage {
 
         deal(address(sUSDe), address(this), 1_000 * 1e18);
         sUSDe.approve(address(pendleRouter), type(uint).max);
+
+        _setLabels();
     }
 
     
@@ -34,7 +36,29 @@ contract RouterTest is Test, Storage {
 
         console.log('SY: ', address(SY));
         console.log('PT: ', address(PT));
+        //---------
 
-        
+        uint sUSDeBalance = sUSDe.balanceOf(address(this));
+        require(sUSDeBalance > 0, 'sUSDeBalance less than 0');
+
+        console.log('expiry: ', sUSDeMarket.expiry());
+        console.log('stamp: ', block.timestamp);
+
+        console.log('sUSDeBalance: ', sUSDeBalance);
+
+        (uint256 netPtOut,,) = pendleRouter.swapExactTokenForPt(
+            address(this), address(sUSDeMarket), 0, defaultApprox, createTokenInputStruct(address(sUSDe), sUSDeBalance), emptyLimit
+        );
+        console.log("netPtOut: ", netPtOut);
     }
+
+
+    //********* */
+    
+    function _setLabels() private {
+        vm.label(address(pendleRouter), 'pendleRouter');
+        vm.label(address(sUSDeMarket), 'sUSDeMarket');
+        vm.label(address(sUSDe), 'sUSDe');
+    }
+    
 }
