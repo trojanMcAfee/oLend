6e71715aefec66c6aa3e6401e033c330dd4afc14
dnyrm <cdgmachado0@gmail.com>
2024-09-15 23:36:10 +0200

    Added SafeERC20

--- contracts/ozRelayer.sol
+++ contracts/ozRelayer.sol
@@ -4,11 +4,14 @@ pragma solidity 0.8.26;
 
 import {IERC20} from "@pendle/core-v2/contracts/interfaces/IPMarket.sol";
 import {IPool} from "@aave/core-v3/contracts/interfaces/IPool.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 import "forge-std/console.sol";
 
 
 contract ozRelayer {
+
+    using SafeERC20 for IERC20;
 
     function borrowInternal(uint amount_, address receiver_, address account_) external {
         IPool aavePool = IPool(0x87870Bca3F3fD6335C3F4ce8392D69350B4fA4E2);
@@ -16,14 +19,7 @@ contract ozRelayer {
         uint variableRate = 2;
 
         aavePool.borrow(address(USDC), amount_, variableRate, 0, account_);
-
-        console.log('usdc bal oz - pre transfer: ', USDC.balanceOf(msg.sender));
-        console.log('msg.sender: ', msg.sender);
-
-        USDC.transfer(msg.sender, amount_);
-
-        console.log('usdc bal oz - post transfer: ', USDC.balanceOf(msg.sender));
-
+        USDC.safeTransfer(msg.sender, amount_);
     }
 
 }
