261858ddeab32880ae1bb47e01d9fb8bb66b8d3a
dnyrm <cdgmachado0@gmail.com>
2024-09-22 18:32:28 +0200

    Changed license comments

--- contracts/State.sol
+++ contracts/State.sol
@@ -1,2 +1,8 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity 0.8.26;
+
+import {AppStorage} from "./AppStorage.sol";
+
+abstract contract State {
+    AppStorage internal s;
+}
